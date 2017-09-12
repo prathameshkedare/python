e5514ecbd0ac5acc5e461cb2d47078bd55fbd4f7
prathameshkedare <31881528+prathameshkedare@users.noreply.github.com>
2017-09-12 13:37:17 +0530

    Add files via upload

---
+++ abc.java
@@ -0,0 +1,12 @@
+class abc
+{
+ public void display_msg()
+ {
+   System.out.println("Hello SYBSc students...");
+  }
+   public static void main(String args[])
+   {
+     abc dd=new abc();
+     dd.display_msg();
+    }
+}
